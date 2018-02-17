77473963e5a05ab8430a0945023b435361b30910
JulienHe <hoshi.graphi@gmail.com>
2018-02-17 17:25:25 +0900

    Fix Test

--- src/app/pages/_error.js
+++ src/app/pages/_error.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Error extends React.Component {
   static getInitialProps({ res, err }) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-    return { statusCode }
+    return { statusCode };
   }
 
   render() {
@@ -16,3 +16,7 @@ export default class Error extends React.Component {
     );
   }
 }
+
+Error.propTypes = {
+  statusCode: PropTypes.string,
+};
