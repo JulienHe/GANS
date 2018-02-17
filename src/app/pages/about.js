83005ad36d18ca4b5563b51440482ba3cb7fdc82
JulienHe <hoshi.graphi@gmail.com>
2018-02-17 22:35:21 +0900

    Routing Client-side + Server-Side

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Head from 'next/head';
 import App from 'components/App';
 import {
@@ -11,7 +12,7 @@ export class About extends PureComponent {
   render() {
     // This is an example to show how Next-routes can work.
     const id = this.props.url.query.id ? `ID: ${this.props.url.query.id}` : 'No id found, sorry';
-    
+
     return (
       <App>
         <Head>
@@ -25,5 +26,9 @@ export class About extends PureComponent {
     );
   }
 }
+
+About.propTypes = {
+  url: PropTypes.object,
+};
 
 export default About;
